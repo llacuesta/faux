e6f2c3179f7dac5babffe186c394dbb284adfda2
Lyco Lacuesta <lycosheen27@gmail.com>
2022-06-12 22:43:21 +0800

    Delete unused react files. Add redirect to /sign-up

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route } from "react-router-dom"
+import { BrowserRouter, Redirect, Route } from "react-router-dom"
 import Signup from "./pages/Signup";
 import Feed from "./pages/Feed";
 import Login from "./pages/Login";
@@ -10,6 +10,9 @@ function App() {
         <div className="App">
             <div className="wrapper">
                 <BrowserRouter>
+                    <Route exact={true} path="/">
+                        <Redirect to="/sign-up" />
+                    </Route>
                     <Route exact={true} path="/sign-up" component={Signup} />
                     <Route exact={true} path="/log-in" component={Login} />
                     <Route exact={true} path="/feed" component={Feed} />
